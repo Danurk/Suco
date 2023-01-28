9bc81bbe4d921cd9e3c9b2c2dca787cf29d37bd2
Carlos Alexandre <carlos.alexandren3@gmail.com>
2023-01-27 21:11:18 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+
+# Inicializa o bot
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+# Comando para tocar uma música do YouTube
+@bot.command()
+async def play(ctx, url):
+    # Cria uma nova conexão de voz
+    voice_channel = ctx.author.voice.channel
+    vc = await voice_channel.connect()
+
+    # Usa a biblioteca youtube_dl para baixar e reproduzir a música
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        vc.play(discord.FFmpegPCMAudio(info['url']))
+        vc.source = discord.PCMVolumeTransformer(vc.source)
+        vc.source.volume = 0.5
+
+# Inicia o bot
+bot.run('MTA2ODU4Mzc2NjI5NTA1MjM5OQ.GCFbTu.T5JM4G4uzRalTNt7s1FmgPZDcyWE-9FdO5A18Y')
+
+
